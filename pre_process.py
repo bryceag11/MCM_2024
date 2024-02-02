b24101cb8cdad8e4afc3a420e068a13dda035fee
Bryce <bagr247@g.uky.edu>
2024-02-02 14:07:04 -0500

    Updating preprocessing

--- pre_process.py
+++ pre_process.py
@@ -3,8 +3,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-# Read the CSV file
+# Read the provided CSV file
 df = pd.read_csv('Wimbledon_featured_matches.csv')
+
+# Read the correct/reference data
+
+# Check if the DataFrames are equal
+
+
+
 
 # feature and target extraction
 features = df.drop('game_victor', axis=1)
